05daf02e90fff20d05d40eac83889abe355536dc
mahak-mhk <77165635+mahak-mhk@users.noreply.github.com>
2022-03-03 13:30:21 +0530

    Add files via upload

---
+++ StarApp.Api/Controllers/AllowanceController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StarApp.Core.Domain.DTOs;
+using StarApp.Core.Domain.Entities;
+using StarApp.Core.Interfaces.Service;
+using System.Security.Claims;
+using System.Text;
+
+namespace StarApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllowanceController : ControllerBase
+    {
+        private readonly IAllowanceService _allowanceService;
+        private readonly IExcelService _excelService;
+
+        public AllowanceController(IAllowanceService allowanceService, IExcelService excelService)
+        {
+            _allowanceService = allowanceService;
+            _excelService = excelService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<AllowancePageDto>> Get([FromQuery]AllowanceFilter filter)
+        {
+            //string UserName = null;
+            //var identity = HttpContext.User.Identity as ClaimsIdentity;
+            //if (identity != null)
+            //{
+            //    UserName = identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            //}
+
+            //if (filter.Name == null && UserName != null) filter.Name = UserName;
+
+            var responseData = await _allowanceService.GetAll(filter);
+            return Ok(responseData);
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Allowance>> Get(int id)
+        {
+            return await _allowanceService.GetById(id);
+        }
+
+        [HttpGet("Download")]
+        public async Task<FileContentResult> Download([FromQuery] string? name)
+        {
+            var dataString = await _excelService.ToCsv(name);
+            var fileName = $"Allowance_{DateTime.Now.ToString()}.csv";
+            var fileType = "text/csv";
+            return File(Encoding.ASCII.GetBytes(dataString), fileType, fileName);
+        }
+
+        [Authorize]
+        [HttpGet("Upload")]
+        public async Task<ActionResult> Upload()
+        {
+            var file = new FileInfo(@"C:\Users\asus\Desktop\test.xlsx");
+            await _excelService.ReadFromExcel(file);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Post([FromBody] Allowance allowance)
+        {
+            _allowanceService.Post(allowance);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Allowance allowance)
+        {
+            var allowanceData = await _allowanceService.Put(id, allowance);
+            if (!allowanceData) return NotFound("Id not found");
+            return Ok("Data updated successfully");
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _allowanceService.Delete(id);
+            if (!response) return NotFound("Id not found");
+            return Ok("Data deleted successfully");
+        }
+    }
+}
---
+++ StarApp.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarApp.Core.Domain.DTOs;
+using StarApp.Core.Domain.DTOs.Employee;
+using StarApp.Core.Interfaces.Service;
+using static StarApp.Core.Domain.Enums.StatusEnum;
+
+namespace StarApp.Api.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IAuthService _authService;
+
+        public EmployeesController(IEmployeeService employeeService, IAuthService authService)
+        {
+            _employeeService = employeeService;
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<EmployeePageDto>> GetEmployees([FromQuery]EmployeeFilter employeeFilter)
+        {
+            return Ok(await _employeeService.Get(employeeFilter));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetEmployeesDto>> GetEmployee(int id)
+        {
+            var employee = await _employeeService.GetById(id);
+            if (employee == null) return BadRequest("This id does not exist");
+            return Ok(employee);
+        }
+
+        //[HttpGet("Names")]
+        //public async Task<ActionResult<IEnumerable<string>>> GetNames()
+        //{
+        //    var employees = await _employeeService.GetNames();
+        //    if (employees == null) return NotFound("No Employees exist in the database");
+        //    return Ok(employees);
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> PostEmployees(PostEmployeesDto postEmployeesDto)
+        {
+            if (await employeeAleadyExist(postEmployeesDto.Email)) return BadRequest("This User Name is already taken.");
+            _employeeService.Post(postEmployeesDto);
+            return Ok("Data updated successfully");
+        }
+
+        [HttpPost("ChangePassword")]
+        public async Task<ActionResult> ChangePassword(EmployeePasswordDto passwordDto)
+        {
+            var employee = await _employeeService.GetEmployeeEntity(passwordDto.Id);
+            if (employee == null) return BadRequest("This Id does not exist");
+            if (!_authService.VarifyPassword(employee, passwordDto.OldPassword)) return BadRequest("Old password is incorrect");
+            var (passwordHash, passwordSalt) = _authService.GetPasswordHash(passwordDto.NewPassword);
+            employee.PasswordSalt = passwordSalt;
+            employee.PasswordHash = passwordHash;
+            _employeeService.Update(employee);
+            return Ok("Password Updated Successfully");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutEmployee(int id, GetEmployeesDto employeeDto)
+        {
+            if (!await _employeeService.Put(id, employeeDto)) return BadRequest("This Id does not exist");
+            return Ok("Data updated successfully");
+        }
+
+        [HttpPut("{id}/Status/{status}")]
+        public async Task<ActionResult> UpdateStatus(int id, Status status)
+        {
+            var isUpdated = await _employeeService.UpdateStatus(id, status);
+            if (!isUpdated) return BadRequest("Id not found");
+            return Ok($"Status updated to {status.ToString()}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            await _employeeService.Delete(id);
+            return Ok("Data deleted successfully");
+        }
+
+        private async Task<bool> employeeAleadyExist(string email)
+        {
+            var employee = await _employeeService.GetByEmail(email);
+            return employee != null;
+        }
+    }
+}
